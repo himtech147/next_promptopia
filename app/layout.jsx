a07e6fd9c0c28a208e800d885c82175947d6f8e4
Himanshu <himtech147@gmail.com>
2024-05-04 21:19:12 +0530

    Project Setup

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptogram",
+  description: "Platform to share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
