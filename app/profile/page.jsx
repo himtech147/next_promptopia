ea8c730f71878bbb79e2055672b9d1429c964391
Himanshu <himtech147@gmail.com>
2024-05-12 08:00:44 +0530

    Update and delete posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -9,17 +9,17 @@ const MyProfile = () => {
   const router = useRouter();
 
   const { data: session } = useSession();
-  const [posts, setPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const res = await fetch(`api/users/${session?.user.id}/posts`);
+        const res = await fetch(`/api/users/${session?.user.id}/posts`);
         const data = await res.json();
 
-        setPosts(data);
+        setMyPosts(data);
       } catch (error) {
-        console.log("Unable to fetch posts", error);
+        console.log("Unable to fetch myPosts", error);
       }
     };
     // only fetch when we have the user logged in here
@@ -30,13 +30,33 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    console.log("delete post", post);
+    const hasConfirmed = window.confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = myPosts.filter(
+          (post_item) => post_item._id != post._id
+        );
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <Profile
       name="My"
       desc="Welcome to your personalised profile page"
-      data={posts}
+      data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
